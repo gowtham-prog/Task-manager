e8a6e0d2db777fb598105b8e919862dad2f65565
gowtham-prog <gowthamkumarvulluri@gmail.com>
2024-07-17 14:28:48 +0530

    added task URLs, altered user URLs

--- Task_Management/apis/urls.py
+++ Task_Management/apis/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import UserCreateAPIView,UserUpdateAPIView,UserRetrieveAPIView, LogoutView
+from .views import UserCreateAPIView,UserUpdateAPIView,UserRetrieveAPIView, LogoutView, TaskCreateAPIView,TaskListAPIView, TaskRetrieveUpdateDestroyAPIView
 urlpatterns = [
-    path("user/create", UserCreateAPIView.as_view(), name="user_create"),
-    path("user/get", UserUpdateAPIView.as_view(), name="user_update"),
-    path("user/update", UserRetrieveAPIView.as_view(), name="user_retrieve"),
+    path("user/create/", UserCreateAPIView.as_view(), name="user_create"),
+    path("user/update/<str:id>/", UserUpdateAPIView.as_view(), name="user_update"),
+    path("user/get/", UserRetrieveAPIView.as_view(), name="user_retrieve"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path("task",TaskCreateAPIView.as_view(), name="task"),
+    path("task/<str:key>/", TaskListAPIView.as_view(), name="task_list"),
+    path("task_detail/<str:id>/", TaskRetrieveUpdateDestroyAPIView.as_view(), name="task_detail"),
 ]
