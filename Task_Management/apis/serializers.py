847b82dc344120e183eee97c5bc03789dfbad5f4
gowtham-prog <gowthamkumarvulluri@gmail.com>
2024-07-17 14:28:06 +0530

    restricted user_type fields to read_only, created nested serializer representation in Task Serializer

--- Task_Management/apis/serializers.py
+++ Task_Management/apis/serializers.py
@@ -7,11 +7,19 @@ from .models import Task, User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = 'username, first_name, last_name, email, password,' 
+        fields = ('id','username', 'first_name', 'last_name', 'email', 'password', 'user_type', 'organization') 
+        read_only_fields = ['user_type']
     
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['created_by'] = UserSerializer(instance.created_by, many=False, allow_null=True, required=False).data
+        data['assigned_to'] = UserSerializer(instance.assigned_to, many=True, allow_null=True, required=False).data
+        
+        return data
 
